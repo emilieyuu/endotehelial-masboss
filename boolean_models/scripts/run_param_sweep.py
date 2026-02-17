1b44c837149f238611ec2afac45163fcbac32319
emilieyuu <yuemilie19@gmail.com>
2026-02-17 16:46:34 +0000

    running all 1D param sweeps together

--- boolean_models/scripts/run_param_sweep.py
+++ boolean_models/scripts/run_param_sweep.py
@@ -9,58 +9,65 @@ from boolean_models.analysis import (
     compute_delta,
     classify_phenotype,
     save_df_to_csv, 
+    generate_ko_model
 )
 
 
-def run_param_sweep(model, result_dir, config, perturbation='WT'):
+def run_param_sweep(base_model, result_dir, config):
     # Extract variables from config
+    perb_cfg = config['perturbations']
     sweep_cfg = config['sensitivity_analysis']
     param_cfg = sweep_cfg['parameter_ranges']
     groups = sweep_cfg['groups']
 
-    # Identify which parameters to sweep based on the profile
-    profile = sweep_cfg['sweep_profiles'][perturbation]
-
     # Initiate list to store parameter sweep results
     result = []
-    
-    # Run parameter sweep for each parameter (for each group)
-    print(f"DEGUG: Starting sweep for perturbation {perturbation}")
-    for group, features in groups.items(): 
-        # For each parameter
-        for p in features['parameters']:
-            name = f"${group}_{p}"
 
-            # Skip parameters in non-exhaustive KO scenarios
-            if profile != "exhaustive" and name not in profile: 
-                continue
-            
-            # Setup sweeping ranges
-            values = np.arange(param_cfg[p]['range'][0], param_cfg[p]['range'][1], param_cfg[p]['step'])
+    for perb, mutation in perb_cfg.items():
+        # Create model of KO scenario
+        model = generate_ko_model(base_model, mutation)
 
-            print(f"DEGUG: Performing sweep for parameter {name} with values: {values}")
+        # Identify which parameters to sweep based on the profile
+        profile = sweep_cfg['sweep_profiles'][perb]
+        
+        # Run parameter sweep for each parameter (for each group)
+        print(f"DEGUG: Starting sweep for perturbation {perb}")
+        for group, features in groups.items(): 
+            # For each parameter
+            for p in features['parameters']:
+                name = f"${group}_{p}"
 
-            # Perform sweep
-            for val in values: 
-                m = model.copy()
-                m.update_parameters(**{name: val})
+                # Skip parameters in non-exhaustive KO scenarios
+                if profile != "exhaustive" and name not in profile: 
+                    continue
+                
+                # Setup sweeping ranges
+                values = np.arange(param_cfg[p]['range'][0], param_cfg[p]['range'][1], param_cfg[p]['step'])
 
-                res = m.run()
-                ss_df = res.get_last_nodes_probtraj()
-                ss_df['param_value'] = val
-                ss_df['param_name'] = name
+                print(f"DEGUG: Performing sweep for parameter {name} with values: {values}")
 
-                delta_df = ss_df.copy()
-                delta_df['delta'] = compute_delta(delta_df, config)
+                # Perform sweep
+                for val in values: 
+                    m = model.copy()
+                    m.update_parameters(**{name: val})
 
-                phenotype_df = delta_df.copy()
-                phenotype_df['phenotype'] = delta_df['delta'].apply(lambda x: classify_phenotype(x, config))
+                    res = m.run()
+                    ss_df = res.get_last_nodes_probtraj()
+                    ss_df['param_value'] = val
+                    ss_df['param_name'] = name
+                    ss_df['perturbation'] = perb
 
-                result.append(phenotype_df)
+                    delta_df = ss_df.copy()
+                    delta_df['delta'] = compute_delta(delta_df, config)
 
-    print(f"DEBUG: Sweep of parameters for {perturbation} completed. ")
+                    phenotype_df = delta_df.copy()
+                    phenotype_df['phenotype'] = delta_df['delta'].apply(lambda x: classify_phenotype(x, config))
+
+                    result.append(phenotype_df)
+
+        print(f"DEBUG: Sweep of parameters for {perb} completed. ")
 
     combined_df = pd.concat(result, ignore_index=True)
-    save_df_to_csv(combined_df, result_dir, f"{perturbation}_param_sweep")
+    save_df_to_csv(combined_df, result_dir, "full_1D_param_sweep")
 
     return combined_df
