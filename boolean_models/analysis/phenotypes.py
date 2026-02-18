0763024b853d72f1cb1c9e553c0cf57fd30d8904
emilieyuu <yuemilie19@gmail.com>
2026-02-18 03:20:27 +0000

    final full 1D and 2D param sweeps completed: changes to config (temp) + separate function and combined processing for sweeps

--- boolean_models/analysis/phenotypes.py
+++ boolean_models/analysis/phenotypes.py
@@ -9,18 +9,6 @@ def compute_delta(df, config):
     #print(f"DEBUG: Computing delta = {rhoC} - {rhoA}")
     return df[rhoC] - df[rhoA]
 
-# Phenotype timeseries
-# def classify_phenotype(prob_df, eps=0.25):
-#     """ Classify into discrete phenotype based on threshold. """
-#     #delta = compute_delta(prob_df)
-#     delta = prob_df['delta']
-#     pheno_df = pd.DataFrame({
-#                     "Failed": (delta < -eps).astype(float),
-#                     "Hyper": (delta > eps).astype(float),
-#                     "Normal": (abs(delta) <= eps).astype(float),
-#                 }, index=prob_df.index)
-
-#     return pheno_df
 
 def classify_phenotype(delta, config):
     """ Compute phenotype based on delta. """
--- boolean_models/scripts/run_param_sweep.py
+++ boolean_models/scripts/run_param_sweep.py
@@ -1,9 +1,8 @@
 #boolean_models/scripts/run_param_sweep.py
 import maboss
 import pandas as pd
-from pathlib import Path
-import yaml
 import numpy as np
+import itertools
 
 from boolean_models.analysis import (
     compute_delta,
@@ -12,62 +11,143 @@ from boolean_models.analysis import (
     generate_ko_model
 )
 
+# --------------------------------------------------
+# Helper Functions
+# --------------------------------------------------
+def build_ranges(sweep_config):
+    # Extract group, range and step info
+    ranges = sweep_config['ranges']
+    groups = sweep_config['groups']
 
-def run_param_sweep(base_model, result_dir, config):
-    # Extract variables from config
-    perb_cfg = config['perturbations']
-    sweep_cfg = config['sensitivity_analysis']
-    param_cfg = sweep_cfg['parameter_ranges']
-    groups = sweep_cfg['groups']
+    # Initiate list to store parameter and values
+    param_dict = {}
 
-    # Initiate list to store parameter sweep results
-    result = []
+    for group in groups:
+        for p, info in ranges.items(): 
+            # Construct parameter name
+            p_name = f"${group}_{p}"
 
-    for perb, mutation in perb_cfg.items():
-        # Create model of KO scenario
-        model = generate_ko_model(base_model, mutation)
+            # Setup sweeping values
+            values = np.arange(info['start'], info['stop'], info['step'])
+            param_dict[p_name] = values
+            
+    return param_dict
 
-        # Identify which parameters to sweep based on the profile
-        profile = sweep_cfg['sweep_profiles'][perb]
+
+# --------------------------------------------------
+# Experiment Runs
+# --------------------------------------------------
+def run_1d_sweeps(base_model, exp_config, perb_config, param_values):
+    """
+    Run all 1D experiments, and return a combined DataFrame with results. 
+    
+    :param base_model: Description
+    :param exp_config: Description
+    :param perb_config: Description
+    :param param_values: Description
+    """
+    # Initiate list to store results
+    results = []
+
+    for perb, params in exp_config.items():
+        # Create model for KO scenaro
+        perb_model = generate_ko_model(base_model, perb_config[perb])
+
+        # Resolve "all" to parameter list
+        if params == 'all': 
+            params = param_values.keys()
+
+        print(f"DEBUG: Starting 1D sweep for perturbation: {perb} with parameter {params}")
+
+        for p in params:
+            # Get values to sweep for parameter
+            values = param_values[p]
+
+            for v in values: 
+                # Setup model with adjusted parameter values
+                m_temp = perb_model.copy()
+                m_temp.update_parameters(**{p: v})
+
+                # Run model and extract steady state probabilities
+                res = m_temp.run()
+                ss_df = res.get_last_nodes_probtraj()
+
+                # Append info columns
+                ss_df[['p_name', 'p_value', 'perb']] = pd.DataFrame([[p, v, perb]], index=ss_df.index)
+
+                results.append(ss_df)
+
+        print("DEBUG: All 1D sweeps completed")
+    
+    # Return dataframe of concatenated results for all 1D experiments
+    return pd.concat(results)
+
+
+def run_2d_sweep(base_model, exp, perb_config, param_values):
+    """
+    Run all 2D experiments, and return a combined DataFrame with results.
+    
+    :param base_model: Description
+    :param exp_config: Description
+    :param perb_config: Description
+    :param param_values: Description
+    """
+    # Initiate list to store results
+    results = []
+    
+    # Extract experiment information
+    exp_name = exp['name']
+    perbs = exp['perturbations']
+    p1, p2 = exp['parameters']
+
+    print(f"DEBUG: Starting 2D sweep experiment {exp_name} with parameters {p1, p2}")
+
+    for perb in perbs:
+        # Create model for KO scenaro
+        perb_model = generate_ko_model(base_model, perb_config[perb])
+
+        # Setup sweeping ranges
+        v1_list = param_values[p1]
+        v2_list = param_values[p2]
         
-        # Run parameter sweep for each parameter (for each group)
-        print(f"DEGUG: Starting sweep for perturbation {perb}")
-        for group, features in groups.items(): 
-            # For each parameter
-            for p in features['parameters']:
-                name = f"${group}_{p}"
+        print(f"DEBUG: Computed values: {p1}: {v1_list}, {p2}: {v2_list}")
+        
+        # Run model for each combination of parameter values (cartesian product)
+        for v1, v2 in itertools.product(v1_list, v2_list):
+            
+            # Setup model with adjusted parameter values
+            m_temp = perb_model.copy()
+            m_temp.update_parameters(**{p1: v1, p2: v2})
 
-                # Skip parameters in non-exhaustive KO scenarios
-                if profile != "exhaustive" and name not in profile: 
-                    continue
-                
-                # Setup sweeping ranges
-                values = np.arange(param_cfg[p]['range'][0], param_cfg[p]['range'][1], param_cfg[p]['step'])
+            # Run model and extract steady state probabilities
+            res = m_temp.run()
+            ss_df = res.get_last_nodes_probtraj()
 
-                print(f"DEGUG: Performing sweep for parameter {name} with values: {values}")
+            # Append info columns
+            ss_df[[p1, p2, 'perb', 'exp_name']] = pd.DataFrame([[v1, v2, perb, exp_name]], index=ss_df.index)
 
-                # Perform sweep
-                for val in values: 
-                    m = model.copy()
-                    m.update_parameters(**{name: val})
+            results.append(ss_df)
+            
+        print(f"DEBUG: Completed {exp_name} sweep for perturbation: {perb}")
 
-                    res = m.run()
-                    ss_df = res.get_last_nodes_probtraj()
-                    ss_df['param_value'] = val
-                    ss_df['param_name'] = name
-                    ss_df['perturbation'] = perb
+    return pd.concat(results)
 
-                    delta_df = ss_df.copy()
-                    delta_df['delta'] = compute_delta(delta_df, config)
+def run_sweeps(base_model, result_dir, sweep_config, sim_config):
+    perb_config = sim_config['perturbations']
+    param_values = build_ranges(sweep_config)
+    exp_1d_cfg = sweep_config['1D_experiments']
+    exp_2d_cfg = sweep_config['2D_experiments']
 
-                    phenotype_df = delta_df.copy()
-                    phenotype_df['phenotype'] = delta_df['delta'].apply(lambda x: classify_phenotype(x, config))
+    sweep_results = {}
+    sweep_results["1D_sweeps"] = run_1d_sweeps(base_model, exp_1d_cfg, perb_config, param_values)
 
-                    result.append(phenotype_df)
+    for exp in exp_2d_cfg:
+        sweep_results[f"2D_{exp['name']}"] = run_2d_sweep(base_model, exp, perb_config, param_values)
+    
+    for name, df in sweep_results.items():
+        df['delta'] = compute_delta(df, sim_config)
+        df['phenotype'] = df['delta'].apply(lambda x: classify_phenotype(x, sim_config))
+        save_df_to_csv(df, result_dir, name)
 
-        print(f"DEBUG: Sweep of parameters for {perb} completed. ")
+    return sweep_results
 
-    combined_df = pd.concat(result, ignore_index=True)
-    save_df_to_csv(combined_df, result_dir, "full_1D_param_sweep")
-
-    return combined_df
